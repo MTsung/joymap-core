097f641d739f92b6a373c8416aa562f4fad113f7
MTsung <MTsung103035@gmail.com>
2023-11-24 19:04:04 +0800

    1.0.3

--- config/notification.php
+++ config/notification.php
@@ -4,11 +4,11 @@ return [
     'line_notify' => [
         'enable' => env('SEND_ERROR_LOG_LINE', false,),
         'url' => 'https://notify-api.line.me/api/notify',
-        'token' => env('LINE_NOTIFY_TOKEN', 'tKTz5wT8uOyHfumGhuGirKxlTVANjNGKq3aHAiDdbwR'),
+        'token' => env('LINE_NOTIFY_TOKEN', ''),
     ],
     'fcm' => [
         'url' => env('FCM_URL', 'https://fcm.googleapis.com/fcm/send'),
-        'token' => env('FCM_KEY', 'AAAAyH5sP2s:APA91bFg6nMzQh6K-H_paG80QZNamK_Hx5YHEMrDr48g0kXGERMzq4V4ypiYIrEePGepLnBQvShfSy49MLQeeGr0SEiS50S7f0xp8UX9UwwIoZMHg-7VlaIT0fSbXf2uw0fcZkzpfmxX'),
+        'token' => env('FCM_KEY', ''),
     ],
     'gorush' => [
         'host' => env('GORUSH_HOST', 'http://localhost'),
