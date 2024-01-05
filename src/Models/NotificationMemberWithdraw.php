e0c8629660f24bf245ecc2d113e8c059f9573f7b
MTsung <MTsung103035@gmail.com>
2024-01-05 15:38:30 +0800

    add NotificationMemberWithdraw Status

--- src/Models/NotificationMemberWithdraw.php
+++ src/Models/NotificationMemberWithdraw.php
@@ -14,6 +14,19 @@ class NotificationMemberWithdraw extends Model
 
     protected $guarded = ['id'];
 
+    // 0 提領申請
+    public const STATUS_WITHDRAW_APPLY = 0;
+    // 1 匯款完成
+    public const STATUS_REMITTANCE_COMPLETED = 1;
+    // 2 預估分潤
+    public const STATUS_ESTIMATED_PROFIT = 2;
+    // 3 未加入方案通知
+    public const STATUS_NOTIFY_REGISTER_NO_PLAN = 3;
+    // 4 付費方案獎勵
+    public const STATUS_PAID_PLAN_REWARD = 4;
+    // 5 未加入回饋通知
+    public const STATUS_NOTIFY_MEMBER_NO_REWARD = 5;
+
     public function getMorphClass(): string
     {
         return $this->getTable();
