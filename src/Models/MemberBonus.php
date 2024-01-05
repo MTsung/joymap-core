9f098e2b30db1a1223cbdba8a63c4387db397505
MTsung <MTsung103035@gmail.com>
2024-01-05 11:23:21 +0800

    MemberMonthBonusPayAmount

--- src/Models/MemberBonus.php
+++ src/Models/MemberBonus.php
@@ -3,9 +3,9 @@
 namespace Mtsung\JoymapCore\Models;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class MemberBonus extends Model
 {
@@ -47,20 +47,13 @@ class MemberBonus extends Model
      * @param null $status
      * @return mixed
      */
-    public static function MemberMonthBonusPayAmount(
-        string $year,
-        string $month,
-        $memberId = null,
-        $status = null
-    ): mixed
+    public static function MemberMonthBonusPayAmount(string $year, string $month, $memberId = null, $status = null): mixed
     {
         $date = Carbon::createFromDate($year, $month);
-        $year = $date->format('Y');
-        $month = $date->format('n');
-        $firstDayofMonth = $date->startOfMonth()->format('Y-m-d 00:00:00');
-        $lastDayofMonth = $date->endOfMonth()->format('Y-m-d 23:59:59');
+        $firstDayOfMonth = $date->startOfMonth()->toDateTimeString();
+        $lastDayOfMonth = $date->endOfMonth()->toDateTimeString();
 
-        if (!isset($status) || is_null($status)) {
+        if (!isset($status)) {
             $status = 0;
         }
         $condition = "
@@ -71,10 +64,28 @@ class MemberBonus extends Model
                 month = $month
         ";
 
-        if (isset($memberId) && !is_null($memberId)) {
+        if (isset($memberId)) {
             $condition .= " AND member_id = $memberId ";
         }
+        //先抓經銷身分
+        $memberDealerBonus = self::selectBonusPayAmount($condition, 1, $firstDayOfMonth, $lastDayOfMonth);
+        //再抓樂粉身分
+        $condition .= " AND relation_level < 5 ";
+        $memberBonus = self::selectBonusPayAmount($condition, 0, $firstDayOfMonth, $lastDayOfMonth);
+        //合併
+        return $memberBonus->concat($memberDealerBonus);
+    }
 
+    /**
+     * 執行每一個有獲得分潤的會員該月實際刷卡金額和分潤金額
+     * @param string $condition 語法
+     * @param bool $dealerGrade 經銷商身分
+     * @param string $firstDayOfMonth 月份(月初)
+     * @param string $lastDayOfMonth 月份(月底)
+     * @return mixed
+     */
+    protected static function selectBonusPayAmount(string $condition, bool $dealerGrade, string $firstDayOfMonth, string $lastDayOfMonth): mixed
+    {
         return self::hydrate(DB::select("
             SELECT
                 mb.member_id as member_id,
@@ -100,12 +111,16 @@ class MemberBonus extends Model
                     FROM
                         pay_logs
                     WHERE
-                        created_at BETWEEN '$firstDayofMonth' AND '$lastDayofMonth'
+                        created_at BETWEEN '$firstDayOfMonth' AND '$lastDayOfMonth'
                     AND
                         user_pay_status = 1
                 ) AS pl
                 ON
                     mb.member_id = pl.member_id
+                LEFT JOIN members AS m
+                ON
+                    mb.member_id = m.id
+                WHERE m.is_joy_dealer = $dealerGrade
                 GROUP BY
                     mb.member_id
                 ORDER BY mb.member_id ASC
