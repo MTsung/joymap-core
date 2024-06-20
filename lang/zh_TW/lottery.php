8347bb9c09d4df10d1c4feced06277764f2a64b1
kenLinTWDD <a861252012@gmail.com>
2024-06-20 17:31:33 +0800

    調整獎項名稱

--- lang/zh_TW/lottery.php
+++ lang/zh_TW/lottery.php
@@ -6,7 +6,7 @@ return [
     'type' => [
         Lottery::TYPE_COMMON_TURNTABLE => '一般轉盤',
         Lottery::TYPE_SPECIAL_TURNTABLE => '黃金轉盤',
-        Lottery::TYPE_COMMON_GACHA => '一般扭蛋',
-        Lottery::TYPE_SPECIAL_GACHA => '黃金扭蛋'
+        Lottery::TYPE_COMMON_SCRATCHCARD => '一般刮刮樂',
+        Lottery::TYPE_SPECIAL_SCRATCHCARD => '黃金刮刮樂'
     ],
 ];
