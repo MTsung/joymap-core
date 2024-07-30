0ff0a4e116830e06a33e0224b4c9edeab67220b0
MTsung <MTsung103035@gmail.com>
2024-07-30 08:16:26 +0800

    ticket lang

---
+++ lang/zh_TW/ticket.php
@@ -0,0 +1,10 @@
+<?php
+
+use Mtsung\JoymapCore\Models\TicketBrand;
+
+return [
+    'brand_source' => [
+        TicketBrand::SOURCE_QWARE => '安源資訊',
+        TicketBrand::SOURCE_MOHIST => '墨攻網路',
+    ],
+];
