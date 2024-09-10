45aeb9300083c8c6f20e0088740553c14b024682
MTsung <MTsung103035@gmail.com>
2024-09-10 14:40:09 +0800

    add status const

--- src/Models/StorePayLogs.php
+++ src/Models/StorePayLogs.php
@@ -11,6 +11,15 @@ class StorePayLogs extends Model
 
     protected $guarded = ['id'];
 
+    // 刷卡失敗
+    public const STATUS_FAIL = 0;
+    // 刷卡成功
+    public const STATUS_SUCCESS = 1;
+    // 無刷卡動作
+    public const STATUS_NO_ACTION = 2;
+    // 退刷
+    public const STATUS_REFUND = 3;
+
     public function storeSubscription(): BelongsTo
     {
         return $this->belongsTo(StoreSubscription::class, 'store_subscription_id');
