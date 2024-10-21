d2783f417dd915407315a395d9134589087966fa
MTsung <MTsung103035@gmail.com>
2024-10-22 07:00:59 +0800

    信件標題修正

--- lang/zh_TW/mail.php
+++ lang/zh_TW/mail.php
@@ -7,7 +7,7 @@ return [
             'update' => '您在「 :name 」訂位資訊已更改。',
             'cancel' => '您在「 :name 」的 訂位已取消',
             'remind' => '提醒您，您有預訂「 :name 」預訂 :date (週:week) :time :people_count 人，請準時前往。',
-            'comment_remind' => '「 :name 」感謝您光臨，邀請您留下評論。',
+            'comment-remind' => '「 :name 」感謝您光臨，邀請您留下評論。',
         ]
     ],
 ];
